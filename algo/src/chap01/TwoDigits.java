54f9f7e99eb149d4bd27612d0dffce54729e633a
yujinoh <0rangesunset@naver.com>
2025-07-14 19:58:55 +0900

    250714 -1

---
+++ algo/src/chap01/TwoDigits.java
@@ -0,0 +1,20 @@
+package chap01;
+
+import java.util.Scanner;
+
+public class TwoDigits {
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int no;
+
+        System.out.println("2자리의 양수를 입력하세요.");
+
+        do{
+            System.out.print("no값: ");
+            no = stdIn.nextInt();
+        }while(no < 10 || no > 99);
+
+        System.out.println("변수 no값은 " + no + "이 되었습니다.");
+    }
+}
